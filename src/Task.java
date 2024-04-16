f4612d71a7478d9c3d61b3cfecd96125d7e111a7
konnan933 <konnan933@gmail.com>
2024-04-16 11:24:01 +0200

    task osztaly

---
+++ src/Task.java
@@ -0,0 +1,14 @@
+public class Task {
+    String name;
+    int prio;
+    int start;
+    int length;
+
+    public Task(String name, int prio, int start, int length){
+        this.name = name;
+        this.prio = prio;
+        this.start = start;
+        this.length = length;
+    }
+
+}
